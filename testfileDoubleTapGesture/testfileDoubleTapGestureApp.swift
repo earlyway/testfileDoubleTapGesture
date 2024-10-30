b09599bf0ea8d08077dd9919d6d5483d1c87076a
tg kim <tiky@kakao.com>
2024-10-30 18:36:57 +0900

    241030 import Realitykit 에러 해결 주석 추가

--- testfileDoubleTapGesture/testfileDoubleTapGestureApp.swift
+++ testfileDoubleTapGesture/testfileDoubleTapGestureApp.swift
@@ -6,8 +6,8 @@
 //
 
 import SwiftUI
-import RealityKit // 이 주석을 풀어버리면 에러가 나오면서 실행이 안됨...' Type 'testfileDoubleTapGestureApp does not conform to protocol 'App' '
-
+import RealityKit // 9번 라인 주석을 풀어버리면 시뮬레이터가 아닌 xcode 에 빨간 에러가 나오면서 build실행이 안됨. ' Type 'testfileDoubleTapGestureApp does not conform to protocol 'App' '
+                    //  9번 라인 주석을 풀었을 때, 에러가 14번 줄의 some Scene 에서 발생함. 14번 줄의 Scene 의 지칭을 SwiftUI.Scene 으로 명확하게 해주면 에러 없음.
 @main
 struct testfileDoubleTapGestureApp: App {
     
